1bce42ac55bb300f66409de9ff6a040de4f965ae
Name <denis_777@ukr.net>
2017-12-12 16:20:51 +0200

    121217

---
+++ customer.php
@@ -0,0 +1,7 @@
+<?php 
+$sql = $_POST["number"];
+$sql2 = $_POST["status"];
+//$sql .= $sql2; 
+echo $sql.$sql2;
+//echo $sq2;
+?>
--- index.php
+++ index.php
@@ -1,17 +1,85 @@
-<?php
-session_start();
-require 'autoload.php';
-//try{
-//var_dump($_POST['number']);
-$controler = new Wnet\Controler\Task();
-$action = $_GET['action'] ? : 'Index';
+<html>
+  <head>
+    <title>Тестовое задание</title>
+    <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+   <script>
 
-$controler->action($action);
-//} catch(\Wnet\Exception\Core $ex) {
-//	echo 'Проблема компонента отображения ';
-//}
-//   catch(\Wnet\Exception\Connect $ex) {
-//	echo 'Нет соединения с базой';
-//}
+$(document).ready(function(){ 
+$("#search_results").slideUp(); 
+    $("#submit").click(function(e){ 
+        e.preventDefault(); 
+        ajax_search(); 
+    }); 
+    $("#status1").change(function(e){ 
+        e.preventDefault(); 
+        ajax_search(); 
+    }); 
+    $("#status2").change(function(e){ 
+        e.preventDefault(); 
+        ajax_search(); 
+    }); 
+    $("#status3").change(function(e){ 
+        e.preventDefault(); 
+        ajax_search(); 
+    }); 
+        $("#number").change(function(e){ 
+        e.preventDefault(); 
+        ajax_search(); 
+    }); 
 
-?>
+
+});
+
+function ajax_search(){ 
+  $.ajax({
+    type: "POST",
+    url: "./customer.php",
+    // The key needs to match your method's input parameter (case-sensitive).
+    data: JSON.stringify({ Markers: markers }),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){alert(data);},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+ /* $("#search_results").show(); 
+  var search_val = $("#number").val(); 
+  var search_status = status();
+   $.post("./customer.php",
+   {
+    number : search_val,
+    status: search_status
+
+   }, 
+   function(data){
+  if (data.length>0){ 
+     $("#search_results").html(data); 
+ } 
+});
+
+function status(){
+ var value = $('input[name=status]:checked').val();
+ return value; 
+}*/
+
+
+} 
+
+</script>
+  </head>
+  <body>
+  <form  method="post" enctype="multipart/form-data">
+  	<input type="text" name="number" id="number"> 
+    <label>work</label><input id="status1" type="radio" name="status" value="work" required>
+    <label>connecting</label><input id="status2" type="radio" name="status" value="connecting" required>
+    <label>disconnected</label><input id="status3" type="radio" name="status" value="disconnected" required>
+  	<input id="submit" type="submit" name="submit" value="Отправить">
+  </form>
+  <p id="search_results">123</p>
+ 
+  </body>
+</html>
+
+
+
